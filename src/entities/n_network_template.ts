8ac27a208bd028c2cba6563d870655f9e21636c9
tanrenjie <1694805460@qq.com>
2025-03-12 18:30:01 +0800

    1

--- src/entities/n_network_template.ts
+++ src/entities/n_network_template.ts
@@ -1,36 +1,46 @@
 import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
 import { CompleteEntity } from '~/common/entity/common.entity'
 import { DictItemEntity } from './dict-item.entity'
+import { ApiProperty } from '@nestjs/swagger'
 
 @Entity('n_network_template')
 export class NNetWorkTemplateEntity extends CompleteEntity {
+  @ApiProperty()
   @Column('text', { name: 'name', comment: '文件夹名称或网络模板名称' })
   name: string
 
+  @ApiProperty()
   @Column('text', { name: 'other_conf', comment: '其他参数-JSON字符串, 根据配置表参数不同', nullable: true })
   other_conf: string
 
+  @ApiProperty()
   @Column('text', { name: 'h_table_id', comment: '调频表ID  逗号分隔的字符串', nullable: true })
   h_table_ids: string
 
+  @ApiProperty()
   @Column('integer', { name: 'type', default: 1, comment: '1-文件夹类型; 2-网络模板类型' })
   type: number
 
+  @ApiProperty()
   @ManyToOne(() => DictItemEntity, { cascade: true, createForeignKeyConstraints: false,})
   @JoinColumn({ name: 'device_type',referencedColumnName: 'value'})
   device_type: DictItemEntity
 
+  @ApiProperty()
   @ManyToOne(() => DictItemEntity, { cascade: true, createForeignKeyConstraints: false,})
   @JoinColumn({ name: 'access_method',referencedColumnName: 'value'})
   access_method: DictItemEntity
 
+  @ApiProperty()
   @ManyToOne(() => DictItemEntity, { cascade: true, createForeignKeyConstraints: false,})
   @JoinColumn({ name: 'waveForm',referencedColumnName: 'value'})
   waveForm: DictItemEntity
 
+  @ApiProperty()
   @Column('text', { name: 'ts_ids', comment: '时序表ID  逗号分隔', nullable: true })
   ts_ids: string
 
+  @ApiProperty()
   @Column('integer', { name: 'pId', comment: '父ID', default: 0 })
   pId: number
 }
--- src/modules/network-template/dto/NetWorkTemplateDTO.ts
+++ src/modules/network-template/dto/NetWorkTemplateDTO.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty } from 'class-validator'
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator'
 
 enum n_network_template_type {
   Folder = 1,
@@ -13,9 +13,12 @@ export class NetWorkTemplateDTO {
   @ApiProperty({ description: '文件名称  或 模板名称', example: '文件名称  或 网络模板名称' })
   name: string
 
-  @ApiProperty({ enum: n_network_template_type, description: '1:文件  或 2:网络模板  不传 默认是1', example: 1, default: 1, required: false })
-  type: number
+  @IsOptional()
+  @IsEnum(n_network_template_type)
+  @ApiProperty({ enum: n_network_template_type, description: '1:文件  或 2:网络模板  不传 默认是1', example: 1, default: 1 })
+  type?: number
 
-  @ApiProperty({ description: 'root节点 不传 默认是0', example: 0, default: 0, required: false })
-  pId: number
+  @IsOptional()
+  @ApiProperty({ description: 'root节点 不传 默认是0', example: 0, default: 0 })
+  pId?: number
 }
