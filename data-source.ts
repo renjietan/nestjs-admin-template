ee39444b64dbf23cacab55bdcb1642c3b8c4ebea
tanrenjie <1694805460@qq.com>
2025-03-28 00:31:13 +0800

    1

--- data-source.ts
+++ data-source.ts
@@ -1,35 +1,33 @@
-// src/data-source.ts
-import { DataSource } from "typeorm";
+// // src/data-source.ts
+// import { DataSource } from "typeorm";
 
-export const AppDataSource = new DataSource({
-  type: "mysql", // 数据库类型（如 mysql、sqlite、mssql 等）
-  host: "localhost", // 数据库地址
-  port: 3306, // 端口（PostgreSQL 默认 5432，MySQL 默认 3306）
-  username: "root", // 用户名
-  password: "123456", // 密码
-  database: "nest-admin", // 数据库名
-  synchronize: true, // 是否自动同步实体到数据库表（生产环境建议关闭）
-  logging: true, // 是否启用 SQL 日志
-  entities: ["dist/modules/**/*.entity{.ts,.js}"], // 实体类（或直接指定路径，如 ["src/entity/**/*.ts"]）
-  // migrations: ["src/migrations/**/*.ts"], // 迁移文件路径
-  subscribers: [], // 订阅者（可选）
-  extra: {
-    ssl: false // 是否启用 SSL（如云数据库可能需要）
-  }
-});
+// export const AppDataSource = new DataSource({
+//   type: "mysql", // 数据库类型（如 mysql、sqlite、mssql 等）
+//   host: "localhost", // 数据库地址
+//   port: 3306, // 端口（PostgreSQL 默认 5432，MySQL 默认 3306）
+//   username: "root", // 用户名
+//   password: "123456", // 密码
+//   database: "nest-admin", // 数据库名
+//   synchronize: true, // 是否自动同步实体到数据库表（生产环境建议关闭）
+//   logging: true, // 是否启用 SQL 日志
+//   entities: ["dist/modules/**/*.entity{.ts,.js}"], // 实体类（或直接指定路径，如 ["src/entity/**/*.ts"]）
+//   // migrations: ["src/migrations/**/*.ts"], // 迁移文件路径
+//   subscribers: [], // 订阅者（可选）
+//   extra: {
+//     ssl: false // 是否启用 SSL（如云数据库可能需要）
+//   }
+// });
 
+// // export const dataSourceOption: DataSourceOptions = {
+// //     type: 'better-sqlite3',
+// //     // database: path.join(__dirname, '..', 'db', 'sqlite3.sqlite3'),
+// //     database: ':memory:',
+// //     synchronize: true,
+// //     dropSchema: true,
+// //     enableWAL: true,
+// //     entities:["dist/modules/**/*.entity{.ts,.js}"]
+// //   };
 
+//   // const dataSource = new DataSource(dataSourceOption);
 
-// export const dataSourceOption: DataSourceOptions = {
-//     type: 'better-sqlite3',
-//     // database: path.join(__dirname, '..', 'db', 'sqlite3.sqlite3'),
-//     database: ':memory:',
-//     synchronize: true,
-//     dropSchema: true,
-//     enableWAL: true,
-//     entities:["dist/modules/**/*.entity{.ts,.js}"]
-//   };
-  
-  // const dataSource = new DataSource(dataSourceOption);
-  
-  // export default dataSource;
+//   // export default dataSource;
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -7,6 +7,7 @@ import { DataSource, DataSourceOptions } from 'typeorm'
 import { env, envBoolean, envNumber } from '~/global/env'
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
+console.log('process.env.NODE_ENV================', process.env.NODE_ENV)
 
 // 当前通过 npm scripts 执行的命令
 const currentScript = process.env.npm_lifecycle_event
@@ -14,11 +15,11 @@ const currentScript = process.env.npm_lifecycle_event
 const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
   host: env('DB_HOST', '127.0.0.1'),
-  port: envNumber('DB_PORT', 3306),
+  port: envNumber('DB_PORT', 3300),
   username: env('DB_USERNAME'),
   password: env('DB_PASSWORD'),
   database: env('DB_DATABASE'),
-  //note: 是否在每次应用程序启动时自动创建数据库模式。 请谨慎使用此选项，不要在生产环境中使用，否则您可能会丢失生产数据
+  // note: 是否在每次应用程序启动时自动创建数据库模式。 请谨慎使用此选项，不要在生产环境中使用，否则您可能会丢失生产数据
   synchronize: envBoolean('DB_SYNCHRONIZE', false),
   // 解决通过 pnpm migration:run 初始化数据时，遇到的 SET FOREIGN_KEY_CHECKS = 0; 等语句报错问题, 仅在执行数据迁移操作时设为 true
   multipleStatements: currentScript === 'typeorm',
